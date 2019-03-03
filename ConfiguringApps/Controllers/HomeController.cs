8e436704492d2aaaf87c9351381b95e310c56a3d
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-03-03 19:07:12 +0100

    Added starting items and Infrastructure folder with few classes.

---
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController: Controller
+    {
+        private UptimeService _uptime;
+
+        public HomeController(UptimeService uptime)
+        {
+            _uptime = uptime;
+        }
+
+        public ViewResult Index() 
+            => View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the index action",
+                ["Uptime"] = $"{_uptime.Uptime}ms"
+            });
+    }
+}
